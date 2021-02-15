9d1fd276642f6b0eb6193e8456da12b0f2acd3bd
杨程 <7702269+yangcheng666@user.noreply.gitee.com>
2021-02-15 18:36:42 +0800

    按钮基本样式

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 轻UI - 一个基于Vue的轻量级UI组件
-## 作者：猪鲜666
+作者：猪鲜666
+
+## 安装
+
+使用本框架之前，请在 css 中开启 border-box
+```
+*{margin: 0;padding: 0;box-sizing: border-box;}
+```
---
+++ src/button.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <button class="q-button">按钮</button>
+</template>
+<script>
+export default {
+    name:'q-button'
+}
+</script>
+<style lang="scss">
+    .q-button{
+        height: var(--button-height);
+        padding: 0 1em;
+        font:inherit;
+        border-radius: var(--border-radius);
+        border:1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover{
+            border-color:var(--border-color-hover);
+        }
+        &:active{
+            background-color: var(--button-active-bg);
+        }
+    }
+
+</style>
